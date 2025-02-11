92b2814aa3a36062648116ac4fd2f04ab8ff30fe
Azizbek <aziz2311e@gmail.com>
2025-02-11 21:37:46 +0500

    binary_search_learn

--- bin/searching_algorithm/binary/learn.dart
+++ bin/searching_algorithm/binary/learn.dart
@@ -17,18 +17,53 @@
 
 void main() {
   List<int> numbers = [1, 3, 5, 7, 9, 11, 15, 18, 21, 24];
-  int target = 11;
+  int target1 = 11;
 
   /// ------ numbers ichidan targetni indexini topishimiz kerak ----------///
-  int? result = binarySearchRecursive(numbers, target, 0, numbers.length - 1);
+  int? result = binarySearchRecursive(numbers, target1, 0, numbers.length - 1);
   if (result != null) {
     print("natija : $result");
   } else {
     print("element topilmadi..");
   }
-  //// natija : 5 ✅ tg:@eshpolatov_azizbek
+  // words
+  List<String> words = [
+    "apple",
+    "banana",
+    "cherry",
+    "grape",
+    "lemon",
+    "mango",
+    "orange",
+    "peach",
+    "pear",
+    "watermelon"
+  ];
+  String target2 = "mango";
+  int? result2 = binarySearchString(words, target2, 0, words.length - 1);
+
+  if (result2 != null) {
+    print("$target2' $result2-indeksda joylashgan");
+  } else {
+    print("So'z topilmadi.");
+  }
 }
 
+// berilgan string royxatdan birorta sozning indexini topish
+int? binarySearchString(
+    List<String> words, String target, int left, int right) {
+  if (left > right) return null;
+  int mid = left + (right - left) ~/ 2;
+  if (words[mid] == target) {
+    return mid;
+  } else if (words[mid].compareTo(target) > 0) {
+    return binarySearchString(words, target, left, mid - 1);
+  } else {
+    return binarySearchString(words, target, mid + 1, right);
+  }
+}
+
+// qidirilayotgan element indexini qaytarish
 int? binarySearchRecursive(List<int> nums, int target, int left, int right) {
   if (left > right) return null;
   int mid = left + (right - left) ~/ 2; //[0:9] 0 + (9-0)~/2 => 4
