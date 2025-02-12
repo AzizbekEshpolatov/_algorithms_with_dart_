696dcba007f93e2fb27b06da30f1daf22e334604
Azizbek <aziz2311e@gmail.com>
2025-02-12 21:11:47 +0500

    quick_sort_example

--- bin/sort/quick_sort/practice.dart
+++ bin/sort/quick_sort/practice.dart
@@ -1,32 +1,33 @@
-//   List<int> numbers = [34, 7, 23, 32, 5, 62, 32, 2];
-
-void main() {
-  List<int> numbers = [34, 7, 23, 32, 5, 62, 32, 2];
-  print(numbers);
-
-  sortedLs(numbers, 0, numbers.length - 1);
-
-  print(numbers);
+void swapHelper(List<int> ls, int i, int j) {
+  int temp = ls[i];
+  ls[i] = ls[j];
+  ls[j] = temp;
 }
 
-void sortedLs(List<int> numbers, int low, int high) {
-  if (low < high) {
-    int pivot = numbers[high];
-    int i = low - 1;
+int partition(List<int> ls, int high, int low) {
+  int i = low - 1;
+  int pivot = ls[high];
 
-    for (int j = low; j < high; j++) {
-      if (pivot > numbers[j]) {
-        i++;
-        int t = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = t;
-      }
+  for (int j = low; j < high; j++) {
+    if (pivot > ls[j]) {
+      i++;
+      swapHelper(ls, i, j);
     }
-    int t = numbers[i + 1];
-    numbers[i + 1] = numbers[high];
-    numbers[high] = t;
-    int pivotIndex = i + 1;
-    sortedLs(numbers, low, pivotIndex - 1);
-    sortedLs(numbers, pivotIndex + 1, high);
   }
+  swapHelper(ls, i + 1, high);
+  return i + 1;
+}
+
+void sortedList(List<int> ls, int high, int low) {
+  if (low < high) {
+    int pivotIndex = partition(ls, high, low);
+    sortedList(ls, high, pivotIndex + 1);
+    sortedList(ls, pivotIndex - 1, low);
+  }
+}
+
+void main() {
+  var ls = [2, 41, 4, 0, 10];
+  sortedList(ls, ls.length - 1, 0);
+  print(ls);
 }
