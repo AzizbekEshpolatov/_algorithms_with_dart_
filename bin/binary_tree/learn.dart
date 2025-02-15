87c373d42c11b591d7b7608bb3ca2efbbb635468
Azizbek <aziz2311e@gmail.com>
2025-02-16 02:16:47 +0500

    binary tree nodes count example

--- bin/binary_tree/learn.dart
+++ bin/binary_tree/learn.dart
@@ -19,15 +19,6 @@ class TreeNode {
 4   5   6
  */
 
-/// Inorder Traversal yozish.
-/// Inorder – bu daraxtdagi eng asosiy yurib chiqish usuli
-void inorderTraversal(TreeNode? node) {
-  if (node == null) return;
-  inorderTraversal(node.left);
-  print(node.value);
-  inorderTraversal(node.right);
-}
-
 void main() {
   TreeNode root = TreeNode(
     1,
@@ -41,14 +32,36 @@ void main() {
   // print("Inorder Traversal:");
   // inorderTraversal(root);
   print("binary tree height:${heightBinaryTree(root)}");
+  print("binary tree nodes count:${countNode(root)}");
 }
+
+/// Binary Tree dagi tugunlar sonini hisoblash
+/// binary tree-dagi barcha node-lar sonini topish methhodi.
+int countNode(TreeNode? node) {
+  if (node == null) return 0;
+  int leftCount = countNode(node.left);
+  int rightCount = countNode(node.right);
+  return 1 + leftCount + rightCount;
+}
+
+/// ------------------------------------------------------------------------ ///
 
 /// Binary Tree balandligini (Height) topish yani  ildiz (root) tugundan pastga
 /// qarab eng uzoq barggacha bo‘lgan yo‘l uzunligi.
-
 int heightBinaryTree(TreeNode? node) {
   if (node == null) return 0;
   int heightLeft = heightBinaryTree(node.left);
   int heightRight = heightBinaryTree(node.right);
   return 1 + max(heightRight, heightLeft);
+}
+
+/// ------------------------------------------------------------------------ ///
+
+/// Inorder Traversal yozish.
+/// Inorder – bu daraxtdagi eng asosiy yurib chiqish usuli
+void inorderTraversal(TreeNode? node) {
+  if (node == null) return;
+  inorderTraversal(node.left);
+  print(node.value);
+  inorderTraversal(node.right);
 }
