2e0c0a02d6627470ef1ea54cda3020dbb2d1b278
Azizbek <aziz2311e@gmail.com>
2025-02-18 17:55:56 +0500

    remove duplicate letter example.

--- bin/greedy/greedy_practice.dart
+++ bin/greedy/greedy_practice.dart
@@ -1,37 +1,46 @@
-// leetcode - 316. Remove Duplicate Letters
-
 class Solution {
   String removeDuplicateLetters(String s) {
-    String result = '';
-    Map<String, int> countingMap = {};
-    Set<String> helperItem = {};
+    /**
+        Har bir harf faqat 1 marta qatnashsin va eng kichik leksikografik
+        tartibda string hosil qilinsin.
+        bcabc -> shunchaki set qilib bca yoki bac qaytarib qoyish xato.
+        leksikografik bolishi uchun result = abc bolishi kerak
+     */
+    // eng songida qaytarishimiz kerak bolgan string.
+    String result_str = '';
+    // Map → Harf keyin yana keladimi, bilish uchun.
+    Map<String, int> helper_map = {};
+    // Set → Qo‘shilgan harfni qayta qo‘shmaslik uchun.
+    Set<String> helper_set = {};
+    // birinchi navbatda map qiymatlanadi.
+    for (int i = 0; i < s.length; i++)
+      helper_map[s[i]] = (helper_map[s[i]] ?? 0) + 1;
 
+    // asosiy logika, bizda map qiymatlanib bolgan.
     for (int i = 0; i < s.length; i++) {
-      String letter = s[i];
-      countingMap[letter] = (countingMap[letter] ?? 0) + 1;
-    }
-
-    for (int i = 0; i < s.length; i++) {
-      String letter = s[i];
-      if (helperItem.contains(letter)) {
-        countingMap[letter] = countingMap[letter]! - 1;
+      var letter = s[i];
+      // set tekshiriladi. Agar setga qoshilgan bolsa element shuchaki count
+      // kamaytirilib, skip qivorishimiz kerak!.
+      if (helper_set.contains(letter)) {
+        helper_map[letter] = helper_map[letter]! - 1;
         continue;
       } else {
-        while (result.isNotEmpty &&
-            result[result.length - 1].compareTo(letter) > 0 &&
-            countingMap[result[result.length - 1]]! > 0) {
-          helperItem.remove(result[result.length - 1]);
-          result = result.substring(0, result.length - 1);
+        // result check qilish kerak, oxirgi elementni tekshirishimiz kerak.
+        // yani eng etibor berish kerak bolgan leksikografik tartib logikasi shu joyda qilinadi.
+        while (result_str.isNotEmpty &&
+            result_str[result_str.length - 1].compareTo(letter) > 0 &&
+            helper_map[result_str[result_str.length - 1]]! > 0) {
+          helper_set.remove(result_str[result_str.length - 1]);
+          result_str = result_str.substring(0, result_str.length - 1);
         }
-        countingMap[letter] = countingMap[letter]! - 1;
-        result += letter;
+        // har doim letter qoshilishi kerak.
+        // letter qoshilgach, Keyin yana keladimi yoki yo‘qmini bilish uchun map-dagi
+        // countni kamaytiramiz.
+        helper_map[letter] = helper_map[letter]! - 1;
+        helper_set.add(letter);
+        result_str += letter;
       }
     }
-    return result;
+    return result_str;
   }
-}
-
-void main() {
-  Solution solution = Solution();
-  print(solution.removeDuplicateLetters("bcabc"));
 }
