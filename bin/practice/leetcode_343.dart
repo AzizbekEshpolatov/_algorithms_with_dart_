1c3a2b41f5a158a028558888f45986bd90c7ad2c
Azizbek <aziz2311e@gmail.com>
2025-03-06 15:00:24 +0500

    leetcode_343 solution

--- bin/practice/leetcode_343.dart
+++ bin/practice/leetcode_343.dart
@@ -1,7 +1,14 @@
 void main() {
   int number = 10; // result 36 = 3*3*4
+  print(numberSum(number));
 }
 
-int numberSum(int number) {
-  return 0;
+int numberSum(int n) {
+  if (n == 2 || n == 3) return n - 1;
+  int c = 1;
+  while (n > 4) {
+    c *= 3;
+    n -= 3;
+  }
+  return c * n;
 }
