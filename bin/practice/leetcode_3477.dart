2086e95002b4f7289ab19e2f0e824eace34599b4
Azizbek <aziz2311e@gmail.com>
2025-03-17 10:14:53 +0500

    yangi masala ishlandi

--- bin/practice/leetcode_3477.dart
+++ bin/practice/leetcode_3477.dart
@@ -2,9 +2,9 @@ class Solution {
   int numOfUnplacedFruits(List<int> fruits, List<int> baskets) {
     List<int> baskets_ls = [...baskets];
 
-    for (int i = 0; i < fruits.length; i++) {
-      for (int j = 0; j < baskets_ls.length; j++) {
-        if (fruits[i] <= baskets[j]) {
+    for(int i=0; i<fruits.length; i++){
+      for(int j=0;j<baskets_ls.length; j++){
+        if(fruits[i]<=baskets_ls[j]) {
           baskets_ls.removeAt(j);
           break;
         }
