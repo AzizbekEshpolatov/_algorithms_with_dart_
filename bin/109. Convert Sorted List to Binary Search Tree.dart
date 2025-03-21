8bc33b73ff96ce4004277e96bac796bce98b2701
Akobir <akobirdilmurodovich@gmail.com>
2025-03-21 23:37:07 +0500

    leetcode  109

---
+++ bin/109. Convert Sorted List to Binary Search Tree.dart
@@ -0,0 +1,24 @@
+class Solution {
+  var arr = <int>[];
+
+  TreeNode? _sortedListToBSTHelper(int startIndex, int len) {
+      if (len == 0) {
+          return null;
+      }
+
+      var leftSubtree = _sortedListToBSTHelper(startIndex, len~/2);
+      var rightSubtree = _sortedListToBSTHelper(startIndex + len ~/ 2 + 1, len~/2 - 1 + len%2);
+      return new TreeNode(arr[startIndex + len ~/ 2], leftSubtree, rightSubtree);
+  }
+
+  TreeNode? sortedListToBST(ListNode? head) {
+      var current = head;
+
+      while (current != null) {
+          arr.add(current.val);
+          current = current.next;
+      }
+
+      return _sortedListToBSTHelper(0, arr.length);
+  }
+}
