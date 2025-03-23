a074bdd275eb06fd957368581e1a9b9772710289
Azizbek <aziz2311e@gmail.com>
2025-03-23 15:32:41 +0500

    inProgress in leetcode 804-problem

--- bin/804. Unique Morse Code Words.dart
+++ bin/804. Unique Morse Code Words.dart
@@ -1,14 +1,49 @@
 class Solution {
   int uniqueMorseRepresentations(List<String> words) {
-    Map<String,String> helperEl = {
-      'a':'.-','b':'-...','c':"-.-.",'d':'-..','e':".",'f':"..-.",'g':"--.",'h':"....",'i':"..",'j':".---",'k':"-.-",'l':".-..",'m':"--",'n':"-.",
-      'o':"---",'p':".--.",'q':"--.-",'r':".-.",'s':"...",'t':"-",'u':"..-",'v':"...-",'w':".--",'x':"-..-",'y':"-.--",'z':"--.."
-    };
-    return 0;
+    Set<dynamic> setMorse = {};
+    for (int i = 0; i < words.length; i++) {
+      List<String> k = [];
+      var s = words[i];
+      for (int j = 0; j < s.length; j++) {
+        if (helperEl.containsKey(s[j])) {
+          k.add(helperEl[s[j]]!);
+        }
+      }
+      setMorse.add(k.join(''));
+    }
+    return setMorse.length;
   }
 }
 
+Map<String, String> helperEl = {
+  'a': '.-',
+  'b': '-...',
+  'c': "-.-.",
+  'd': '-..',
+  'e': ".",
+  'f': "..-.",
+  'g': "--.",
+  'h': "....",
+  'i': "..",
+  'j': ".---",
+  'k': "-.-",
+  'l': ".-..",
+  'm': "--",
+  'n': "-.",
+  'o': "---",
+  'p': ".--.",
+  'q': "--.-",
+  'r': ".-.",
+  's': "...",
+  't': "-",
+  'u': "..-",
+  'v': "...-",
+  'w': ".--",
+  'x': "-..-",
+  'y': "-.--",
+  'z': "--.."
+};
 void main() {
   Solution solution = Solution();
-  print(solution.uniqueMorseRepresentations(["gin","zen","gig","msg"]));
+  print(solution.uniqueMorseRepresentations(["gin", "zen", "gig", "msg"]));
 }
