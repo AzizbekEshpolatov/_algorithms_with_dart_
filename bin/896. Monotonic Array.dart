0ba623652d73a7160fcb06b0462d7c8154014162
Azizbek <aziz2311e@gmail.com>
2025-03-23 15:19:56 +0500

    dart easy solution

--- bin/896. Monotonic Array.dart
+++ bin/896. Monotonic Array.dart
@@ -1,14 +1,10 @@
 class Solution {
   bool isMonotonic(List<int> nums) {
-    bool resultOne = true;
-    bool resultTwo = true;
+    bool resultOne = false;
+    bool resultTwo = false;
     for (int i = 1; i < nums.length; i++) {
-      if (nums[i - 1] > nums[i]) {
-        resultOne = false;
-      }
-      if (nums[i - 1] < nums[i]) {
-        resultTwo = false;
-      }
+      if (nums[i - 1] > nums[i]) resultOne = true;
+      if (nums[i - 1] < nums[i])  resultTwo = true;
     }
     return !(resultOne && resultTwo);
   }
